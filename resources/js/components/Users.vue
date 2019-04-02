64b3ffbc72f947d73b289be702628bbd1e421e11
sallsaidoudev <sallsaidoudev@gmail.com>
2019-04-02 16:06:03 +0200

    first commit

---
+++ resources/js/components/Users.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="container">
+        <div class="row mt-5">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-header">
+                        <h3 class="card-title">Users information</h3>
+
+                        <div class="card-tools">
+                            <button class="btn btn-success"><i class="fa fa-plus"></i> Ajouter</button>
+                        </div>
+                    </div>
+                    <!-- /.card-header -->
+                    <div class="card-body table-responsive p-0">
+                        <table class="table table-hover">
+                            <tbody><tr>
+                                <th>ID</th>
+                                <th>Name</th>
+                                <th>Email</th>
+                                <th>Type</th>
+                                <th>Operations</th>
+                            </tr>
+                            <tr>
+                                <td>183</td>
+                                <td>John Doe</td>
+                                <td>11-7-2014</td>
+                                <td><span class="tag tag-success">Approved</span></td>
+                                <td><a class="btn btn-dark" href="#">
+                                    <i class="fa fa-edit"></i>
+                                </a>
+                                    /
+                                    <a class="btn btn-danger" href="#">
+                                        <i class="fa fa-trash"></i>
+                                    </a>
+                                </td>
+                            </tr>
+                            </tbody></table>
+                    </div>
+                    <!-- /.card-body -->
+                </div>
+                <!-- /.card -->
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Users"
+    }
+</script>
