b09794e5cae865126c27902fda6a92facd55f138
Mamadou Saidou Sall <mamadou-saidou.sall@ubat.fr>
2019-04-08 13:27:20 +0200

    Mise en place de laravel passport(authentification api)

--- resources/js/app.js
+++ resources/js/app.js
@@ -35,6 +35,7 @@ Vue.use(VueRouter);
 let routes = [
     { path: '/dashboard', component: require('./components/dashboard.vue').default},
     { path: '/profile', component: require('./components/profile.vue').default },
+    { path: '/developer', component: require('./components/developer.vue').default },
     { path: '/users', component: require('./components/Users.vue').default },
 ]
 
@@ -68,10 +69,26 @@ window.Fire=new Vue()
  * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
  */
 
+Vue.component(
+    'passport-clients',
+    require('./components/passport/Clients.vue').default
+);
+
+Vue.component(
+    'passport-authorized-clients',
+    require('./components/passport/AuthorizedClients.vue').default
+);
+
+Vue.component(
+    'passport-personal-access-tokens',
+    require('./components/passport/PersonalAccessTokens.vue').default
+);
+
 // const files = require.context('./', true, /\.vue$/i);
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default));
 
 Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
